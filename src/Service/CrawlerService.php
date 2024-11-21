e5f278bb876d5e89418cb130936df8d6150f2964
fadilxcoder <rosunmungurudm@gmail.com>
2024-11-21 19:26:23 +0400

    crawl 2024tv.ru

--- src/Service/CrawlerService.php
+++ src/Service/CrawlerService.php
@@ -20,75 +20,46 @@ class CrawlerService
     {
         $this->client->setMaxRedirects(2);
         $baseUrl = $this->url;
-        $webPageCrawler = $this->client->request('GET', $baseUrl);
-        $matches = [];
-        $webPageCrawler->filter('ul#events > li')->each(function ($node) use (&$matches) {
-            $matchTitle = trim($node->filter('a')->first()->text());
-            $streamUrls = $node->filter('ul li.subitem1 a')->each(function ($streamNode) {
-                return $streamNode->attr('href');
-            });
+        // $webPageJson= $this->client->request('GET', $baseUrl . 'data.php');
+        $json = file_get_contents($baseUrl . 'data.php');
+        // var_dump(json_decode($json));die;
+        $matches = json_decode($json, true);
+
+        // $webPageCrawler->filter('.table-dark > li')->each(function ($node) use (&$matches) {
+        //     $matchTitle = trim($node->filter('a')->first()->text());
+        //     $streamUrls = $node->filter('ul li.subitem1 a')->each(function ($streamNode) {
+        //         return $streamNode->attr('href');
+        //     });
         
-            $matches[$matchTitle] = $streamUrls;
-        });
+        //     $matches[$matchTitle] = $streamUrls;
+        // });
 
         # Build db parent level
         $inMemoryDatabase = [];
         $i = 0;
-        foreach ($matches as $matchName => $matchUrl) {
+        foreach ($matches as $matchKey => $matchValue) {
+            // var_dump($matchValue);die;
+            $title = sprintf('%s vs %s', $matchValue['home'], $matchValue['away']);
+            $liveStreams = [];
+
+            foreach ($matchValue['streams'] as $streams) {
+                $liveStreams[] = [
+                    'stream_url' => $baseUrl . 'ply/2/' . $streams['ch'],
+                    'flag' => 'https://www.shareicon.net/data/128x128/2015/08/08/82248_media_16x16.png',
+                    'channel_name' => sprintf('%s vs %s', $streams['ch'], $streams['lang'])
+                ];
+            }
+
             $inMemoryDatabase[$i] = [
                 'id' => uniqid(),
-                'title' => $matchName,
-                'browser_uuid' => $matchUrl,
+                'title' => $title,
+                'browser_uuid' => $baseUrl . 'live/foot/' . $matchValue['league'] . '/' . $title,
                 'sport_name' => '-',
                 'sport_flag' => 'https://img.icons8.com/?size=48&id=iU4rpa9QGXp0&format=png',
                 'date_time' => "0000/00/00 à 00:00",
+                'live' => $liveStreams
             ];
             $i++;
-        }
-
-        foreach ($inMemoryDatabase as $key => $data) {
-            foreach ($data['browser_uuid'] as $linkKey => $linkValue) {
-                $isoCode = substr($linkValue, -2);
-                $eachPagecrawler = $this->client->request('GET', $linkValue);
-
-                # Extract the <script> block that contains the "videos" variable
-                $scriptTag = $eachPagecrawler->filter('script')->each(function ($node) {
-                    if (strpos($node->text(), 'var videos =') !== false) {
-                        return $node->text();
-                    }
-                });
-
-                # Look for the script that contains the "videos" variable
-                foreach ($scriptTag as $script) {
-                    if ($script) {
-                        $scriptContent = $script;
-                        break;
-                    }
-                }
-
-                # Check if we found the "videos" variable
-                if ($scriptContent && preg_match('/var videos = (.*?);/', $scriptContent, $matches)) {
-
-                    # Replace single quotes with double quotes
-                    $jsonString = str_replace("'", '"', $matches[1]);
-
-                    #  Remove trailing commas before the closing brackets using a regular expression
-                    $jsonString = preg_replace('/,(\s*[}\]])/', '$1', $jsonString);
-                    $videosArray = json_decode($jsonString, true);
-
-                    if (isset($videosArray['SUB'])) {
-                        foreach ($videosArray['SUB'] as $video) {
-                            $inMemoryDatabase[$key]['live'][] = [
-                                'stream_url' => $video['code'],
-                                'flag' => 'https://www.shareicon.net/data/128x128/2015/08/08/82248_media_16x16.png',
-                                'channel_name' => strtoupper($isoCode) . ' - ' . $video['server'],
-                            ];
-                        }
-
-                    }
-                }
-            }
-
         }
 
         return $inMemoryDatabase;
